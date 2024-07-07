c2fb713d99b9afd67fcac372368811704f3a834a
anasim <anasim@egain.com>
2024-07-07 09:40:51 +0530

    Code till igniting our app.

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const heading = React.createElement('h1', {
     id:"heading",
     xyz: "abc"
@@ -16,6 +19,12 @@ console.log(heading);
 
 
 // props are children + attribute that we passed in
+
+
+// when face issues with parcel execution then 
+// remove its cache using rm -rf .cache dist node_modules
+
+
 
 {/* <div id="parent">
     <div id="child">
@@ -43,6 +52,39 @@ const parent = React.createElement('div', {id : "parent"},
 // React.createElement is a function that takes 3 arguments
 // and the third argument is the children that you wanted to pass in
 
+
 const domRoot = ReactDOM.createRoot(document.getElementById('root'));
 
 domRoot.render(parent);
+
+
+// what is the difference between caret and tilde
+// transitive dependencies
+// when you download a package and the package also has a dependency
+// to run, that's why it is called transitive dependenciews
+// caret - ^ - it will update the minor and patch version - minor upgrades
+// tilde - ~ - it will update the patch version - major upgrades
+
+// package.lock.json has the exact version of the package that you have installed
+
+// browserList
+
+// node modules are collecton of all the packages/dependencies that you have installed
+// whatever you can generate, don't put it on the git
+
+// npm -- install the package 
+// npx -- run/execute the package
+
+const userData = fetch('https://api.github.com/users/AsifNasim')
+
+userData
+.then( resp => {
+   return resp.json();
+})
+.then( data => {
+    console.log(data);
+})
+
+
+
+
---
+++ README.md
@@ -0,0 +1,22 @@
+# Namaste JS 🚀
+
+# Parcel
+ - Dev Build
+ - local Server
+ - HMR - Hot Module Replacement
+ - File Watching Algorithm - written in C++
+ - Caching - Fast building 
+ - Image Optimisation
+ - Minification 
+ - Bundling
+ - compress the file for production 
+ - Consistent Hashing - Read about it.
+ - Code splitting
+ - Differential Bundling - support to older version
+ - Diagnostic
+ - Good Error Handling
+ - Gives support to Https as well along with http in case you wanted to test something which requires SSL.
+ - Tree Shaking - remove unused code
+ - Transpilation
+ - Different Dev and Prod Build
+ 
